67af241da800fe31a1dbb00defe6f91c140eb0fa
Lena Zheng <lzhengem@gmail.com>
2017-11-09 12:22:45 -0800

    11/9/17

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test
+{
+   //-----------------------------------------------------------------
+   //  number 11 on test
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+
+		final int MAX = 25, LIMIT = 100;
+		int num1 = 12, num2 = 25, num3 = 87;
+
+		if (num3 == 87 || num2 > num1 && MAX > LIMIT)
+			System.out.println("apple");
+		System.out.println("orange");
+   }
+}
